020d8f9202006a0804a3bdb665c4b465b431768f
janardan <jpethani11@gmail.com>
2021-02-14 21:28:44 +0530

    Added signup/login/logout/getuserdata with authentication

--- src/app.js
+++ src/app.js
@@ -1,27 +1,10 @@
-const path = require('path')
+require('./db/conn') //* to run file
 const express = require('express')
-const hbs = require('hbs')
+const userRouter = require('./routers/user')
 
 const app = express()
-const port = process.env.PORT || 3000
 
-//Define paths for Express config
-const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+app.use(express.json())
+app.use(userRouter)
 
-//Setup handlebars engine and view location
-app.set("view engine", "hbs")
-//express searches templates in views by default so to change it..
-app.set("views", viewsPath)
-hbs.registerPartials(partialsPath)
-app.use(express.static(publicDirPath))
-
-app.get("/", (req, res) => {
-    res.render('index')
-})
-
-app.listen(port, () => {
-    // console.log(publicDirPath)
-    console.log('Server is up and running on ' + port)
-})
+module.exports = app
---
+++ src/routers/user.js
@@ -0,0 +1,127 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+
+router.get('/test', (req, res) => {
+    res.send('From a new file')
+})
+
+router.post("/user/signup", async (req, res) => {
+    const user = new User(req.body)
+    console.log(user);
+
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+    // user.save().then((result) => {
+    //     res.status(201).send(result)
+    // }).catch((err) => {
+    //     res.status(400).send(err)
+    // })
+})
+
+router.post('/user/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/user/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+
+        await req.user.save()
+
+        res.send()
+
+    } catch (e) {
+        res.status(500).send({ error: "Error" + e })
+    }
+})
+
+router.post('/user/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+
+        await req.user.save()
+
+        res.send()
+
+    } catch (e) {
+        res.status(500).send({ error: "Error" + e })
+    }
+})
+
+//* finding/reading users and tasks
+//* middleware fun. added : auth
+router.get('/user/me', auth, async (req, res) => {
+
+    res.send(req.user)
+
+    // try {
+    //     const users = await User.find({})
+    //     res.send(users)
+    // } catch (e) {
+    //     res.status(500).send()
+    // }
+
+    // User.find({}).then((result) => {
+    //     res.send(result)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // })
+})
+
+//* updating endpoints use the PATCH HTTP method
+router.patch('/user/me', auth, async (req, res) => {
+    // req.body keys to array of keys
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+
+        //by this middleware will be executed
+        await req.user.save()
+
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/user/me', auth, async (req, res) => {
+    try {
+        // const user = await User.findByIdAndDelete(req.user._id)
+
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+
+
+module.exports = router
